bf3d6fe8382605a76ecd433f365d3966c0eb46ad
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-05-11 12:09:28 +0200

    create folder Admin and setting all file and link

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')
+    ->name('admin')
+    ->prefix('admin')
+    ->namespace('Admin')
+    ->group(function(){
+
+        Route::get('/home', 'HomeController@index')
+        ->name('home');
+});
+
