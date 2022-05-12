8ef58fb8d9ea8fedaf67df7ca3c50f938a33a75e
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-05-12 10:52:21 +0200

    modification post index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,12 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<ul>
-    @foreach ($posts as $post)
-    <li>
-        <h3>{{ $post->title }}</h3>
-        <p>{{ $post->slug }}</p>
-    </li>        
-    @endforeach
-</ul>
+
+<div class="container">
+    <div class="contain mb-3">
+        <a class="btn btn-primary" href="{{ route('admin.posts.create') }}">Nuovo Post</a>
+    </div>
+
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Data pubblicazione</th>
+            <th scope="col">Data creazione</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>            
+            @foreach ($posts as $post)
+            
+            <tr>
+              <td>{{ $post->id }}</td>
+              <td>{{ $post->title }}</td>
+              <td>{{ $post->slug }}</td>
+              <td>{{ $post->published_at }}</td>
+              <td>{{ $post->created_at }}</td>
+              <td>
+                  <a class="btn btn-small btn-warning" href="{{ route('admin.posts.edit', $post ) }}">Modifica</a>
+              </td>
+            </tr>
+
+            @endforeach
+        </tbody>
+      </table>
+</div>
+
 @endsection
