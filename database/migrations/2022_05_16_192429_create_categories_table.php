d83354c667c6652d15b9bbc0ad63761f00b9abb2
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-05-16 21:59:00 +0200

    setting migration categories

--- database/migrations/2022_05_16_192429_create_categories_table.php
+++ database/migrations/2022_05_16_192429_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name',150);
+            $table->string('slug',180)->unique();
             $table->timestamps();
         });
     }
